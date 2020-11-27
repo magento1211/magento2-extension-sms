c2eddd521ddb84affbcd4b69a7f3b0f808d49040
Magento1211 <hoffmanotto16@gmail.com>
2020-11-27 16:51:04 +0000

    Merged PR 25974: Open SMS Settings in new tab
    ## What's being changed
    
    In this PR we opening Chat settings buttons in a new tab including authorization
    
    ## Why it's being changed
    
    In some browsers iframe didn't work as expected
    
    ## How to review / test this change
    
    - Go to Stores > Configuration > Developer and connect via oauth
    https://dev.azure.com/dotdigital/ec/_git/ec-core-magento2-extension/pullrequest/25971 (checkout to this branch also)
    - If you are connected SMS settings button should be opening a new logged in EC tab in chat sections
    - If not SMS settings button should be opening a new login EC page
    
    Related work items: #129493

--- Block/Adminhtml/Config/SettingsButton.php
+++ Block/Adminhtml/Config/SettingsButton.php
@@ -2,10 +2,46 @@
 
 namespace Dotdigitalgroup\Sms\Block\Adminhtml\Config;
 
+use Dotdigitalgroup\Email\Helper\Config as EmailConfig;
+use Dotdigitalgroup\Email\Helper\OauthValidator;
+use Magento\Backend\Block\Template\Context;
 use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\View\Helper\SecureHtmlRenderer;
 
 class SettingsButton extends \Magento\Config\Block\System\Config\Form\Field
 {
+    const SMS_SETTINGS_URL = 'account/settings/sms';
+
+    /**
+     * @var EmailConfig
+     */
+    private $emailConfig;
+
+    /**
+     * @var OauthValidator
+     */
+    private $oauthValidator;
+
+    /**
+     * SettingsButton constructor.
+     * @param Context $context
+     * @param OauthValidator $oauthValidator
+     * @param EmailConfig $emailConfig
+     * @param array $data
+     * @param SecureHtmlRenderer|null $secureRenderer
+     */
+    public function __construct(
+        Context $context,
+        OauthValidator $oauthValidator,
+        EmailConfig $emailConfig,
+        array $data = [],
+        ?SecureHtmlRenderer $secureRenderer = null
+    ) {
+        $this->emailConfig = $emailConfig;
+        $this->oauthValidator = $oauthValidator;
+        parent::__construct($context, $data, $secureRenderer);
+    }
+
     /**
      * @param AbstractElement $element
      * @return string
@@ -17,7 +53,7 @@ class SettingsButton extends \Magento\Config\Block\System\Config\Form\Field
             ->createBlock(\Magento\Backend\Block\Widget\Button::class)
             ->setType('button')
             ->setLabel(__('Edit SMS settings'))
-            ->setOnClick("#")
+            ->setOnClick(sprintf("window.open('%s','_blank')", $this->getButtonUrl()))
             ->toHtml();
     }
 
@@ -30,5 +66,21 @@ class SettingsButton extends \Magento\Config\Block\System\Config\Form\Field
     {
         $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
         return parent::render($element);
+    }
+
+    /**
+     * @return string
+     */
+    private function getButtonUrl()
+    {
+        return $this->oauthValidator->createAuthorisedEcUrl($this->getSmsSettingsUrl(), 'false');
+    }
+
+    /**
+     * @return string
+     */
+    private function getSmsSettingsUrl()
+    {
+        return $this->emailConfig->getRegionAwarePortalUrl() . self::SMS_SETTINGS_URL;
     }
 }
